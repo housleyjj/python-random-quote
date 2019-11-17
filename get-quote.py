8ad9ed07e97d43e259f897254af3764b991c9d9e
housleyjj <housleyjj@gmail.com>
2019-11-17 05:16:24 -0800

    Add automatic appending to quotes.txt

--- get-quote.py
+++ get-quote.py
@@ -3,7 +3,11 @@ import random
 def primary():
 #   print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  quotes = ["Hello World!", "I walk the line.", "Nawdang", "Do the next thing"]
+
+  f = open("quotes.txt", 'r+')
+  for quote in quotes:
+    f.write(quote + "\n")
   quotes = f.readlines()
   f.close()
 
